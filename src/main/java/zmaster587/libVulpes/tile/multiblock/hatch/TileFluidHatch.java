6e9f37355d91cf04e97060360ef126d367c33359
kaduvill <johansj@uia.no>
2026-03-05 19:06:32 +0100

    revert back

--- src/main/java/zmaster587/libVulpes/tile/multiblock/hatch/TileFluidHatch.java
+++ src/main/java/zmaster587/libVulpes/tile/multiblock/hatch/TileFluidHatch.java
@@ -108,27 +108,18 @@ public class TileFluidHatch extends TilePointer implements IFluidHandlerInternal
 	}
 
 	
-
-
 	@Override
-	@Nullable
-	public FluidStack drain(@Nullable FluidStack resource, boolean doDrain) {
-		if (resource == null || resource.getFluid() == null || resource.amount <= 0) return null;
+	public FluidStack drain(FluidStack resource, boolean doDrain) {
 
-		if (!resource.isFluidEqual(this.fluidTank.getFluid())) {
-			return null;
-		} else {
-			FluidStack fluidStack = this.fluidTank.drain(resource.amount, doDrain);
+		if(resource.isFluidEqual(fluidTank.getFluid())) {
+			FluidStack fluidStack = fluidTank.drain(resource.amount, doDrain);
+			while(useBucket(0, getStackInSlot(0)));
 
-			while (this.useBucket(0, this.getStackInSlot(0))) { }
-
-			if (this.world != null) {
-				this.world.notifyNeighborsOfStateChange(this.pos, this.getBlockType(), true);
-			}
+			world.notifyNeighborsOfStateChange(pos,this.getBlockType(), true);
 			return fluidStack;
 		}
+		return null;
 	}
-
 
 	@Override
 	public FluidStack drain(int maxDrain, boolean doDrain) {
